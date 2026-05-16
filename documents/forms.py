c7c656a86449335d73f13a920a934036495bc15f
ToniKulkov <toni.kulkov@bk.ru>
2026-05-16 18:01:57 +0800

    on branch main

--- documents/forms.py
+++ documents/forms.py
@@ -1,6 +1,30 @@
 from django import forms
 from .models import Document
-
+from django.core.exceptions import ValidationError
+import magic
+MAX_FILE_SIZE = 40 * 1024 * 1024 
+def validate_file_size(value):
+    if value.size > MAX_FILE_SIZE:
+        raise ValidationError(f'Файл слишком большой. Максимальный размер: {MAX_FILE_SIZE / (1024 * 1024)} МБ.')
+def validate_file_mime_type(value):
+    file_header = value.read(1024)
+    value.seek(0)
+    mime_type = magic.from_buffer(file_header, mime=True)
+    allowed_mimes = [
+        'application/pdf',                                      
+        'application/msword',                                   
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 
+        'application/vnd.ms-excel',                             
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',       
+        'application/vnd.ms-powerpoint',                        
+        'application/vnd.openxmlformats-officedocument.presentationml.presentation' 
+    ]
+    
+    if mime_type not in allowed_mimes:
+        raise ValidationError(
+            f'Недопустимый формат файла (определен как: {mime_type}). '
+            'Разрешены только документы PDF, Word, Excel, Powerpoint'
+        )
 class DocumentUploadForm(forms.ModelForm):
     class Meta:
         model = Document
@@ -12,3 +36,5 @@ class DocumentUploadForm(forms.ModelForm):
             'class': 'form-file-input',
             'accept': '.pdf,.doc,.docx,.xls,.xlsx'
         })
+        self.fields['file'].validators.append(validate_file_mime_type)
+        self.fields['file'].validators.append(validate_file_size)
